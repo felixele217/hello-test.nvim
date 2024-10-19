f490404b955fca5faccc7745b919a07ccbfc7f54
felixele217 <droese.felix@gmail.com>
2024-10-19 14:04:35 +0200

    run all has desired keybinds

--- lua/hello-world.lua
+++ lua/hello-world.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.setup = function()
-    vim.keymap.set('n', '<leader>hw', function()
+    vim.keymap.set('n', '<leader>ha', function()
         M.runTests()
     end)
 
@@ -16,17 +16,25 @@ M.runFile = function()
     vim.cmd('vsplit | terminal')
     vim.cmd('startinsert')
 
+
     -- close terminal on pressing enter
     vim.api.nvim_buf_set_keymap(0, 't', '<CR>', '<C-\\><C-n>:q<CR>', {
         noremap = true,
         silent = true
     })
 
-    local cmdBase = "./vendor/bin/pest"
+    vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', 'A<CR>', {
+        noremap = true,
+        silent = true
+    })
+
+    local cmdBase = "./vendor/bin/phpunit"
 
     local cmd = cmdBase .. " " .. file
 
     vim.api.nvim_chan_send(vim.b.terminal_job_id, cmd .. "\n")
+
+    vim.cmd('stopinsert')
 end
 
 M.runTests = function()
@@ -39,8 +47,15 @@ M.runTests = function()
         silent = true
     })
 
-    local cmd = "./vendor/bin/pest"
+    vim.api.nvim_buf_set_keymap(0, 't', 'k', '<C-\\><C-n><C-\\><C-n>', { noremap = true, silent = true })
+
+    vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', ':q<CR>', { noremap = true, silent = true })
+
+    local cmd = "php artisan test"
+    -- local cmd = "./vendor/bin/phpunit"
     vim.api.nvim_chan_send(vim.b.terminal_job_id, cmd .. "\n")
+
+    -- vim.cmd('stopinsert')
 end
 
 return M
