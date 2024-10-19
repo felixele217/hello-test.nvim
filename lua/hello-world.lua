154d0911e64f0aa533b84c15dc66f43182379e2b
felixele217 <droese.felix@gmail.com>
2024-10-19 14:27:13 +0200

    refactor functionality

--- lua/hello-world.lua
+++ lua/hello-world.lua
@@ -1,61 +1,59 @@
-local M = {}
+local M                         = {}
 
-M.setup = function()
+-- local root_dir_cmd = "./vendor/bin/phpunit"
+local root_dir_cmd              = "php artisan test"
+
+local open_terminal             = function()
+    vim.cmd('vsplit | terminal')
+    vim.cmd('startinsert')
+end
+
+local register_keymaps          = function()
     vim.keymap.set('n', '<leader>ha', function()
-        M.runTests()
+        M.run_tests()
     end)
 
     vim.keymap.set('n', '<leader>hf', function()
-        M.runFile()
+        M.run_file()
     end)
 end
 
-M.runFile = function()
-    local file = vim.fn.expand("%")
-
-    vim.cmd('vsplit | terminal')
-    vim.cmd('startinsert')
-
-
-    -- close terminal on pressing enter
-    vim.api.nvim_buf_set_keymap(0, 't', '<CR>', '<C-\\><C-n>:q<CR>', {
-        noremap = true,
-        silent = true
-    })
-
-    vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', 'A<CR>', {
-        noremap = true,
-        silent = true
-    })
-
-    local cmdBase = "./vendor/bin/phpunit"
-
-    local cmd = cmdBase .. " " .. file
-
-    vim.api.nvim_chan_send(vim.b.terminal_job_id, cmd .. "\n")
-
-    vim.cmd('stopinsert')
-end
-
-M.runTests = function()
-    vim.cmd('vsplit | terminal')
-    vim.cmd('startinsert')
-
-    -- close terminal on pressing enter
-    vim.api.nvim_buf_set_keymap(0, 't', '<CR>', '<C-\\><C-n>:q<CR>', {
-        noremap = true,
-        silent = true
-    })
-
+local register_terminal_keymaps = function()
+    -- enter vim mode on pressing k
     vim.api.nvim_buf_set_keymap(0, 't', 'k', '<C-\\><C-n><C-\\><C-n>', { noremap = true, silent = true })
 
+    -- close terminal from terminal mode pressing enter
+    vim.api.nvim_buf_set_keymap(0, 't', '<CR>', '<C-\\><C-n>:q<CR>', { noremap = true, silent = true })
+
+    -- close terminal from normal mode pressing enter
     vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', ':q<CR>', { noremap = true, silent = true })
+end
 
-    local cmd = "php artisan test"
-    -- local cmd = "./vendor/bin/phpunit"
+local run_command               = function(cmd)
     vim.api.nvim_chan_send(vim.b.terminal_job_id, cmd .. "\n")
+end
 
-    -- vim.cmd('stopinsert')
+M.setup                         = function()
+    register_keymaps()
+end
+
+
+M.run_file = function()
+    local file = vim.fn.expand("%")
+
+    open_terminal()
+    register_terminal_keymaps()
+
+    local cmd = root_dir_cmd .. " " .. file
+
+    run_command(cmd)
+end
+
+M.run_tests = function()
+    open_terminal()
+    register_terminal_keymaps()
+
+    run_command(root_dir_cmd)
 end
 
 return M
