834beddcbd21d932161f2b6a54a7ec4d94a8bd29
felixele217 <droese.felix@gmail.com>
2024-10-19 15:53:07 +0200

    wip: better config

--- lua/hello-world.lua
+++ lua/hello-world.lua
@@ -1,21 +1,13 @@
 local M                         = {}
 
--- local root_dir_cmd = "./vendor/bin/phpunit"
-local root_dir_cmd              = "php artisan test"
+local root_dir_cmd              = "./vendor/bin/phpunit"
+-- local root_dir_cmd              = "php artisan test"
+--
+local ts_utils                  = require("nvim-treesitter.ts_utils")
 
 local open_terminal             = function()
     vim.cmd('vsplit | terminal')
     vim.cmd('startinsert')
-end
-
-local register_keymaps          = function()
-    vim.keymap.set('n', '<leader>ha', function()
-        M.run_tests()
-    end)
-
-    vim.keymap.set('n', '<leader>hf', function()
-        M.run_file()
-    end)
 end
 
 local register_terminal_keymaps = function()
@@ -27,33 +19,101 @@ local register_terminal_keymaps = function()
 
     -- close terminal from normal mode pressing enter
     vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', ':q<CR>', { noremap = true, silent = true })
+
+    -- make terminal buffer big in terminal mode
+    vim.api.nvim_buf_set_keymap(0, 't', "'", '<C-\\><C-n><C-\\><C-n><C-W>|', { noremap = true, silent = true })
+
+    -- make terminal buffer big in vim mode
+    vim.api.nvim_buf_set_keymap(0, 'n', "'", '<C-W>|', { noremap = true, silent = true })
 end
 
-local run_command               = function(cmd)
+local setup_terminal            = function()
+    open_terminal()
+    register_terminal_keymaps()
+end
+
+
+local register_keymaps = function()
+    vim.keymap.set('n', '<leader>ta', function()
+        M.run_tests()
+    end)
+
+    vim.keymap.set('n', '<leader>tf', function()
+        M.run_file()
+    end)
+
+    vim.keymap.set('n', '<leader>tt', function()
+        M.run_test_at_cursor()
+    end)
+end
+
+
+local run_command        = function(cmd)
     vim.api.nvim_chan_send(vim.b.terminal_job_id, cmd .. "\n")
 end
 
-M.setup                         = function()
+M.setup                  = function()
     register_keymaps()
 end
 
-
-M.run_file = function()
+M.run_file               = function()
     local file = vim.fn.expand("%")
 
-    open_terminal()
-    register_terminal_keymaps()
+    setup_terminal()
 
     local cmd = root_dir_cmd .. " " .. file
 
     run_command(cmd)
 end
 
-M.run_tests = function()
-    open_terminal()
-    register_terminal_keymaps()
+M.run_tests              = function()
+    setup_terminal()
 
     run_command(root_dir_cmd)
+end
+
+local function_at_cursor = function()
+    local node = ts_utils.get_node_at_cursor()
+
+    if not node then
+        return nil
+    end
+
+    while node do
+        if node:type() == 'method_declaration' then
+            break
+        end
+
+        node = node:parent()
+    end
+
+    if node ~= nil then
+        local child = node:child(2)
+        local child_text = vim.treesitter.get_node_text(child, 0)
+
+        return child_text
+    end
+end
+
+local save_to_register   = function(cmd)
+    local escaped_cmd = vim.fn.escape(cmd, "'")
+    vim.cmd("let @X = '" .. escaped_cmd .. "'")
+end
+
+M.run_test_at_cursor     = function(ta)
+    local function_name = function_at_cursor()
+
+    if function_name == nil then
+        return
+    end
+
+    setup_terminal()
+
+    local cmd = root_dir_cmd .. " --filter=" .. function_name
+
+    run_command(cmd)
+
+    save_to_register(cmd)
 end
 
 return M
